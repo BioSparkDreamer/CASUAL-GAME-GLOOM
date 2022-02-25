360035e81e50bb4cd06dbc23141830a9dd3c53bf
Peyton Paul <peyton.z.paul@gmail.com>
2022-02-25 18:03:28 -0500

    Player Controller & Movement

--- Gloom!/Gloom!/Assets/Scripts/PlayerController.cs
+++ Gloom!/Gloom!/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,94 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static PlayerController instance;
+
+    [Header("Object Variables")]
+    public Rigidbody2D theRB;
+    public Camera camTrans;
+
+    [Header("Movement Variables")]
+    public float moveSpeed = 5f;
+    private Vector2 moveInput, mouseInput;
+    private Vector3 moveHorizontal, moveVertical, rotationAmount;
+    public float mouseSensitivity = 1;
+    private float maxAngle = 150, minAngle = 20;
+
+    [Header("Shooting Variables")]
+    public GameObject bulletEffect;
+    public int currentAmmo;
+    public float nextTimeToFire;
+    private float nextTimeCounter;
+
     void Start()
     {
-        
+        theRB = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+
+        MouseMovement();
+
+        CameraMovement();
+
+        Shooting();
+    }
+
+    void FixedUpdate()
+    {
+        theRB.velocity = (moveHorizontal + moveVertical) * moveSpeed;
+    }
+
+    void Movement()
+    {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        moveHorizontal = transform.up * -moveInput.x;
+        moveVertical = transform.right * moveInput.y;
+    }
+
+    void MouseMovement()
+    {
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y") * mouseSensitivity);
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,
+        transform.rotation.eulerAngles.y,
+        transform.rotation.eulerAngles.z - mouseInput.x);
+    }
+
+    void CameraMovement()
+    {
+        rotationAmount = camTrans.transform.localRotation.eulerAngles + new Vector3(0f, mouseInput.y, 0f);
+
+        camTrans.transform.localRotation = Quaternion.Euler(rotationAmount.x, Mathf.Clamp(rotationAmount.y, minAngle, maxAngle),
+        rotationAmount.z);
+    }
+
+    void Shooting()
+    {
+        if ((Input.GetButton("Shoot") || Input.GetAxis("ShootingTrigger") > 0.5) && nextTimeCounter <= 0)
+        {
+            Ray ray = camTrans.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Instantiate(bulletEffect, hit.point, Quaternion.identity);
+            }
+            else
+            {
+                Debug.Log("I am not looking at anything");
+            }
+
+            nextTimeCounter = nextTimeToFire;
+        }
+
+        if (nextTimeCounter > 0)
+        {
+            nextTimeCounter -= Time.deltaTime;
+        }
     }
 }
